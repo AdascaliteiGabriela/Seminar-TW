9b3bf0e63f5aedebe311de722c44de72e1b68379
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2025-11-19 22:34:57 +0200

    adaugat pasul 4

--- Seminarul 2/Pasul4.js
+++ Seminarul 2/Pasul4.js
@@ -1,4 +1,12 @@
-function arrayNumere(array, ...args)
+function arrayNumere(array)
 {
-    
+    const numere=[];
+    for(let i=0;i<array.length;i++)
+        numere.push(array[i]);
+    return numere;
 }
+
+const vect1 =[1,2,3];
+const vect2=arrayNumere(vect1);
+for(let i=0;i<vect2.length;i++)
+        console.log(vect2[i]);
